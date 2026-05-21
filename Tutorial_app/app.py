eddf7199edfbd8745efd4782f3e887006c0b57de
ramkumar-2002 <ram544new@gmail.com>
2026-05-21 17:29:57 +0530

    app updated with db table creation code

--- Tutorial_app/app.py
+++ Tutorial_app/app.py
@@ -10,6 +10,8 @@ app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URL
 
 db = SQLAlchemy(app)
 
+with app.app_context():
+    db.create_all()
 class User(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
